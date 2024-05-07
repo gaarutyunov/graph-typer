d8905e57e00d15b672158f2b789fbf2b526ad528
German Arutyunov <germanarutyunov@gmail.com>
2024-05-07 04:20:51 +0200

    legacy ddp

--- scripts/train-d.sh
+++ scripts/train-d.sh
@@ -7,7 +7,6 @@ DATASET_ROOT=$2
 MODEL_ARCH=$3
 CKPTS_PATH=$4
 BATCH_SIZE=$5
-PORT=$6
 
 export NCCL_SOCKET_IFNAME=eth
 
@@ -15,9 +14,7 @@ fairseq-train \
 --dataset-root "$DATASET_ROOT" \
 --user-dir ../graph_coder \
 --num-workers 0 \
---distributed-port=$PORT \
---ddp-backend=fully_sharded \
---cpu-offload \
+--ddp-backend=legacy_ddp \
 --dataset-name $DATASET_NAME \
 --task node_classification \
 --user-data-dir ../graph_coder/data \
@@ -29,7 +26,7 @@ fairseq-train \
 --prenorm \
 --num-classes 100 \
 --attention-dropout 0.0 --act-dropout 0.1 --dropout 0.0 \
---optimizer cpu_adam --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
+--optimizer adam --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
 --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
 --lr 2e-4 --end-learning-rate 1e-9 \
 --batch-size "$BATCH_SIZE" \
