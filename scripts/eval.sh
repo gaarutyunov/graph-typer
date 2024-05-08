b8fc21de0d23a22c61359b5864e1f284c261cb02
German Arutyunov <germanarutyunov@gmail.com>
2024-05-09 00:33:39 +0200

    num classes 10000

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -25,7 +25,7 @@ PYTHONPATH=. python graph_coder/evaluate/evaluate.py \
 --performer-feature-redraw-interval 100 \
 --stochastic-depth \
 --prenorm \
---num-classes 100 \
+--num-classes 10000 \
 --attention-dropout 0.0 --act-dropout 0.1 --dropout 0.0 \
 --optimizer adam --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
 --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
