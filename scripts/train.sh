9f872aaf18ff974fbf00b97cf0efbf357d4faa6c
German Arutyunov <germanarutyunov@gmail.com>
2024-05-10 21:59:48 +0200

    warmup updates

--- scripts/train.sh
+++ scripts/train.sh
@@ -26,7 +26,7 @@ fairseq-train \
 --num-classes "$NUM_CLASSES" \
 --attention-dropout 0.0 --act-dropout 0.1 --dropout 0.0 \
 --optimizer adam --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
---lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+--lr-scheduler polynomial_decay --power 1 --warmup-updates 1000 --total-num-update 1000000 \
 --lr 2e-4 --end-learning-rate 1e-9 \
 --batch-size "$BATCH_SIZE" \
 --data-buffer-size 20 \
