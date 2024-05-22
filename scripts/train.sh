f8db68b570af9d04dc45be51f09cf53f7d2937c9
German Arutyunov <germanarutyunov@gmail.com>
2024-05-22 11:47:32 +0200

    scheduler

--- scripts/train.sh
+++ scripts/train.sh
@@ -36,8 +36,8 @@ fairseq-train \
 --prenorm \
 --num-classes "$NUM_CLASSES" \
 --attention-dropout 0.0 --act-dropout 0.1 --dropout 0.0 \
---optimizer adam --adam-eps 1e-8 --clip-norm 25.0 --weight-decay 0.005 \
---lr-scheduler polynomial_decay --power 1 --warmup-updates 1000 --total-num-update 1000000 \
+--optimizer adam --adam-eps 1e-8 --clip-norm 25.0 --weight-decay 0.05 \
+--lr-scheduler polynomial_decay --power 0.5 --warmup-updates 1000 --total-num-update 10000 \
 --lr 1e-2 --end-learning-rate 1e-5 \
 --batch-size "$BATCH_SIZE" \
 --data-buffer-size 20 \
